ef9dafb964881ac22c7d416fdf5291aac15afd77
Voldemort230102 <lyj_230102@outlook.com>
2024-08-20 15:24:16 +0800

    main

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
+# -*-coding:utf-8-*-
 import requests
-url = "https://coding-oj.gaotu100.com/api/file/upload-avatar"
-r = requests.post(url, files={'image': open('./image/avatar.jpg', 'rb')})
+import json
+url = "https://coding-oj.gaotu100.com/api/get-user-home-info?uid=cc92409361844d5ca1d9f93778c45d55&username=郭梓朋"
+h = {
+        "user-agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36 Edg/127.0.0.0"
+}
+r = requests.get(url,headers=h)
+print(r)
+print(r.text)
